4a1788a7162d8df97431a96759433ca4cda3fe4e
ngchgkckjckghcghkflug <135410750+ngchgkckjckghcghkflug@users.noreply.github.com>
2024-07-06 17:32:07 +0530

    a

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // Choose a cache name
 const cacheName = 'cache-v1';
 // List the files to precache
-const precacheResources = ['/dump', '/dump/main.html', '/dump/android-chrome-512x512.png', '/dump/android-chrome-192x192.png','/dump/manifest.json'];
+const precacheResources = ['https://ngchgkckjckghcghkflug.github.io/dump/main.html'];
 
 // When the service worker is installing, open the cache and add the precache resources to it
 self.addEventListener('install', (event) => {
