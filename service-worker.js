1ef9afec55d7137f32e37bcf32699788d72f944e
ngchgkckjckghcghkflug <135410750+ngchgkckjckghcghkflug@users.noreply.github.com>
2024-07-06 18:11:28 +0530

    github.dev

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,12 @@
-// Choose a cache name
-const cacheName = 'cache-v1';
-// List the files to precache
-const precacheResources = ['https://ngchgkckjckghcghkflug.github.io/dump/main.html',];
-
-// When the service worker is installing, open the cache and add the precache resources to it
-self.addEventListener('install', (event) => {
-  console.log('Service worker install event!');
-  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)));
-});
-
-self.addEventListener('activate', (event) => {
-  console.log('Service worker activate event!');
-});
-
-// When there's an incoming fetch request, try and respond with a precached resource, otherwise fall back to the network
-self.addEventListener('fetch', (event) => {
-  console.log('Fetch intercepted for:', event.request.url);
-  event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      if (cachedResponse) {
-        return cachedResponse;
-      }
-      
-    }),
-  );
-});
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open('your-cache-name').then(function(cache) {
+        return cache.addAll([
+          // your list of cache keys to store in cache
+          'main.html',
+          'mainfest.json',
+          // etc.
+        ])
+      })
+    );
+  });
