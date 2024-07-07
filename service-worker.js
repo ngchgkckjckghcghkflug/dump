a8d69d2de751faa9532a4cc64301d2acba7c2ec8
ngchgkckjckghcghkflug <135410750+ngchgkckjckghcghkflug@users.noreply.github.com>
2024-07-07 11:33:00 +0530

    a

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,8 @@
 const cacheName = 'js13kPWA-v1';
 const appShellFiles = [
-  '/dump/main.html'
+  '/dump/main.html',
+  '/dump/manifest.json',
+  'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap'
   
 ];
 const contentToCache = appShellFiles;
