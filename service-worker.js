18890615156809f9d7b4c45fef640e4a84694d4a
ngchgkckjckghcghkflug <135410750+ngchgkckjckghcghkflug@users.noreply.github.com>
2024-07-10 21:39:24 +0530

    a

--- service-worker.js
+++ service-worker.js
@@ -36,7 +36,7 @@ async function registerPeriodicNewsCheck() {
   const registration = await navigator.serviceWorker.ready;
   try {
     await registration.periodicSync.register("get-latest-news", {
-      minInterval: 24 * 60 * 60 * 1000,
+      minInterval: 60 * 1000,
     });
   } catch {
     console.log("Periodic Sync could not be registered!");
