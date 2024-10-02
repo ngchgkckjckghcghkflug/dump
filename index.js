dc0de258c5d0fe5dd4b31c31bba3b9935d2068dc
ngchgkckjckghcghkflug <135410750+ngchgkckjckghcghkflug@users.noreply.github.com>
2024-10-02 06:08:27 +0000

    fixed and made bugs

--- index.js
+++ index.js
@@ -1,3 +1,115 @@
+function getHeight(promptText = "Enter the height in feet",mode=0) {
+    const the_height = prompt(promptText);
+    const parse = (/^(?:[0-9]{1,}'[0-9]{1,2})|(?:^[0-9]{1,}$)$/g).exec(the_height);
+    if (parse !== null) {
+        if (parse[0].includes("'") && parse[0].split("'").length == 2){
+            if (mode == 1){
+                return [Number.parseFloat(parse[0].split("'")[0])+(Number.parseFloat(parse[0].split("'")[1])/12),the_height]
+            } else {
+                return Number.parseFloat(parse[0].split("'")[0])+(Number.parseFloat(parse[0].split("'")[1])/12)
+            }
+        } else {
+            if (mode == 1){
+                return [Number.parseFloat(parse[0]),the_height]
+            } else {
+                return Number.parseFloat(parse[0])
+            }
+        }
+    } else {
+        mylog("Please enter a valid height.");
+    }
+}
+function stop() {
+    throw new Error("stopped execution")
+}
+function parseStr(a) {
+    if (a.includes("'") && a.split("'").length == 2){
+        return Number.parseFloat(a.split("'")[0])+(Number.parseFloat(a.split("'")[1])/12)
+    } else {
+        return Number.parseFloat(a)
+    }
+}
+function getWeight() {
+    let weight = Number.parseFloat(prompt("Enter the weight in kg: "));
+    if (isNaN(weight)) {
+        mylog("Please enter a valid weight.");
+    }else {
+        return weight
+    }
+}
+
+function calculateBMI(height  ,weight) {
+    return  [weight/Math.pow((height * 30.48) / 100, 2), height, weight];
+    
+}
+function describeBMI(array) {
+    const [bmi ,height ,weight] = array;
+    let bmiString = `Your Body Mass Index is ${round2(bmi)}\n`
+    if (bmi <= 18.5) {
+        return bmiString+"You are underweight.";
+    } else if (bmi <= 24.9) {
+        return bmiString+"You are healthy.";
+    } else if (bmi <= 29.9) {
+        return bmiString+`You are over weight.
+        To be healthy you need to lose ${round2(weight - bmi2kg(24.9, height))} kg
+        To be healthy your weight should be ${bmi2kg(24.9, height)} kg`;
+    } else if (bmi > 29.9) {
+        return bmiString+`You are obese.
+        To be healthy your BMI should be between 18.5 and 24.9
+        To be healthy you need to lose ${round2(weight - bmi2kg(24.9, height))} kg
+        To be healthy your weight should be ${bmi2kg(24.9, height)} kg`;
+    } 
+}
+
+document.getElementById("calc").addEventListener("click", function () {
+    div.innerHTML = "";
+    let heighta= getHeight("Enter the height in feet",1) ?? stop() 
+    let height = heighta[1]
+    let weight = getWeight() ??stop()
+    if (localStorage.getItem("height") != null) {
+        localStorage.setItem("height",localStorage.getItem("height")+"@"+`{ "Date":"${todayDate}","Weight":${weight},"Height":"${height}"}`)
+    } else {
+        localStorage.setItem("height",`{ "Date":"${todayDate}","Weight":${weight},"Height":"${height}"}`);
+    }
+    mylog( describeBMI(calculateBMI(heighta[0] ,weight)) );
+})
+document.getElementById("savheight").addEventListener("click", function () {
+    div.innerHTML = ""; 
+    let h = getHeight("Enter height to save",1)
+    if (h != undefined){
+        localStorage.setItem('sheight',h[1]);
+    }
+});
+
+document.getElementById("recalc").addEventListener("click", function () {
+    div.innerHTML = "";
+    if (localStorage.getItem("sheight") !== null){
+        let weight = getWeight() ?? stop()
+        let height = localStorage.getItem("sheight")
+        if (localStorage.getItem("height") != null) {
+            localStorage.setItem("height",localStorage.getItem("height")+"@"+`{ "Date":"${todayDate}","Weight":${weight},"Height":"${height}"}`)
+        } else {
+            localStorage.setItem("height",`{ "Date":"${todayDate}","Weight":${weight},"Height":"${height}"}`);
+        }
+        mylog(describeBMI(calculateBMI(Number.parseFloat(parseStr(localStorage.getItem("sheight"))),weight)));
+    }else {
+        mylog("Please set a height.")
+    }
+});
+document.getElementById("chistory").addEventListener("click", function () {
+    table.innerHTML = "";
+    div.innerHTML = "";
+});
+
+document.getElementById("history").addEventListener("click", function () {
+    table.innerHTML = "";
+    div.innerHTML = "";
+    const dates = localStorage.getItem("height").split("@").map(i => JSON.parse(i));
+    generateTable(table, dates);
+    generateTableHead(table, Object.keys(dates[0]));
+    
+});
+
 const date = new Date();
 var todayDate = `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`;
 
@@ -35,87 +147,3 @@ function generateTable(table, data) {
         }
     }
 }
-
-
-function getHeight() {
-    const the_height = prompt("Enter the height in feet: ");
-    const parse = (/^(?:[0-9]{1,}'[0-1]{1,2})|(?:^[0-9]{1,}$)$/g).exec(the_height);
-    if (parse !== null) {
-        if (parse[0].includes("'") && parse[0].split("'").length == 2){
-            calculate(Number.parseFloat(parse[0].split("'")[0])+(Number.parseFloat(parse[0].split("'")[1])/12))
-        } else {
-            calculate(Number.parseFloat(parse[0]));
-        }
-    } else {
-        mylog("Please enter a valid height.");
-    }
-}
-function calculate(height) {
-    const weight = Number.parseFloat(prompt("Enter the weight in kg: "));
-    if (isNaN(weight)) {
-        mylog("Please enter a valid weight.");
-        return;
-    }
-    const the_BMI = weight / Math.pow((height * 30.48) / 100, 2);
-    mylog(`Your Body Mass Index is ${round2(the_BMI)}`);
-    if (the_BMI <= 18.5) {
-        mylog("You are underweight.");
-    } else if (the_BMI <= 24.9) {
-        mylog("You are healthy.");
-    } else if (the_BMI <= 29.9) {
-        mylog(`You are over weight.
-        To be healthy you need to lose ${round2(weight - bmi2kg(24.9, height))} kg
-        To be healthy your weight should be ${bmi2kg(24.9, height)} kg`);
-    } else if (the_BMI > 29.9) {
-        mylog(`You are obese.
-        To be healthy your BMI should be between 18.5 and 24.9
-        To be healthy you need to lose ${round2(weight - bmi2kg(24.9, height))} kg
-        To be healthy your weight should be ${bmi2kg(24.9, height)} kg`);
-    }
-    if (localStorage.getItem("height") != null) {
-        localStorage.setItem("height",localStorage.getItem("height")+"@"+`{ "Date":"${todayDate}","Weight":${weight},"Height":${height}}`)
-    } else {
-        localStorage.setItem("height",`{ "Date":"${todayDate}","Weight":${weight},"Height":${height}}`);
-    }
-}
-
-document.getElementById("calc").addEventListener("click", function () {
-    div.innerHTML = "";
-    getHeight();
-});
-document.getElementById("savheight").addEventListener("click", function () {
-    div.innerHTML = "";
-    const a = (/^(?:[0-9]{1,}'[0-9]{1,2})|(?:^[0-9]{1,}$)$/g).exec(prompt("Enter the height to save in feet: "));
-    if (a !== null) {
-        if (a[0].includes("'") && a[0].split("'").length == 2){
-           localStorage.setItem('sheight',Number.parseFloat(a[0].split("'")[0])+(Number.parseFloat(a[0].split("'")[1])/12))
-        } else {
-            localStorage.setItem('sheight',Number.parseFloat(a[0]));
-        }
-    } else {
-        mylog("Please enter a valid height.")
-    }
-    
-    
-});
-document.getElementById("recalc").addEventListener("click", function () {
-    div.innerHTML = "";
-    if (localStorage.getItem("sheight") !== null){
-        calculate(Number.parseFloat(localStorage.getItem("sheight")));
-    }else {
-        mylog("Please set a height.")
-    }
-});
-document.getElementById("chistory").addEventListener("click", function () {
-    table.innerHTML = "";
-    div.innerHTML = "";
-});
-
-document.getElementById("history").addEventListener("click", function () {
-    table.innerHTML = "";
-    div.innerHTML = "";
-    const dates = localStorage.getItem("height").split("@").map(i => JSON.parse(i));
-    generateTable(table, dates);
-    generateTableHead(table, Object.keys(dates[0]));
-    
-});
